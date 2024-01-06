dfe05599cae010ca504d038d43fbb1d4d8ceebab
Mrinmay Das <mrinmaydas2604@gmail.com>
2024-01-06 23:44:53 +0530

    bigs fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from training import learn
 app = Flask(__name__)
 
 @app.route("/predict/<float:a>/<float:b>/<float:c>/<float:d>")
-def calculate_sum(a,b,c,d):
+def predict(a,b,c,d):
     result = learn(a, b, c, d)
     return jsonify(result)
 
